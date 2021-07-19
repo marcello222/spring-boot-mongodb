4e1a426fecc788df7623906d8e7e3aaa0d7615fd
Marcello <marcelloeduardo222@gmail.com>
2021-07-19 16:03:32 -0300

    Insercao de usuario com POST

--- src/main/java/com/marcello/mongodb/services/UserService.java
+++ src/main/java/com/marcello/mongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.marcello.mongodb.domain.User;
+import com.marcello.mongodb.dto.UserDTO;
 import com.marcello.mongodb.repository.UserRepository;
 import com.marcello.mongodb.services.exception.ObjectNotFoundException;
 
@@ -25,4 +26,11 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
 }
