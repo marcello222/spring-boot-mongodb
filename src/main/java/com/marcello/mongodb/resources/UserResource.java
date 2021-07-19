dd767900070be3ea95aa451fe9953dfdf777ce76
Marcello <marcelloeduardo222@gmail.com>
2021-07-19 16:39:39 -0300

    Atualização de usuario com PUT

--- src/main/java/com/marcello/mongodb/resources/UserResource.java
+++ src/main/java/com/marcello/mongodb/resources/UserResource.java
@@ -51,5 +51,14 @@ public class UserResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();		
 	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
+	public ResponseEntity<UserDTO> update( @RequestBody UserDTO objDto, @PathVariable String id){
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+		
+	}
 
 }
