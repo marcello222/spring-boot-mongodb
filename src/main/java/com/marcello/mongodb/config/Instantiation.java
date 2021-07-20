6085a305810c741f68c00c9fa86c95e475e7ed82
Marcello <marcelloeduardo222@gmail.com>
2021-07-20 10:58:36 -0300

    Referenciando os posts do usuaio

--- src/main/java/com/marcello/mongodb/config/Instantiation.java
+++ src/main/java/com/marcello/mongodb/config/Instantiation.java
@@ -41,6 +41,9 @@ public class Instantiation implements CommandLineRunner {
 		Post post2 = new Post(null, sdf.parse("23/12/2018"), "Bom dia", "Acordei cedo", new AuthorDTO(maria));
 
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 
 	}
 
