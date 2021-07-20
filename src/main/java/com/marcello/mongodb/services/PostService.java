b953395f966d8191047f2ac5f1b69fe41ee6deb4
Marcello <marcelloeduardo222@gmail.com>
2021-07-20 11:10:33 -0300

    Obtendo post por id

---
+++ src/main/java/com/marcello/mongodb/services/PostService.java
@@ -0,0 +1,26 @@
+package com.marcello.mongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marcello.mongodb.domain.Post;
+import com.marcello.mongodb.repository.PostRepository;
+import com.marcello.mongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
+	
+}
