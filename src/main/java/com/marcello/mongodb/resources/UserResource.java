f111fd98e8f09c5814caf6c18a01ff22cdc8c56f
Marcello <marcelloeduardo222@gmail.com>
2021-07-20 11:05:13 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/marcello/mongodb/resources/UserResource.java
+++ src/main/java/com/marcello/mongodb/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.marcello.mongodb.domain.Post;
 import com.marcello.mongodb.domain.User;
 import com.marcello.mongodb.dto.UserDTO;
 import com.marcello.mongodb.services.UserService;
@@ -60,5 +61,12 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 		
 	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());		
+	}
+	
 
 }
